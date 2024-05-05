97b97ddc3b67c94be40c509a0a5a7a7e34d22020
musfique113 <musfique113@gmail.com>
2024-05-05 23:48:32 +0600

    [FIX] : Fix database empty issue

--- lib/core/services/local_database/database_provider.dart
+++ lib/core/services/local_database/database_provider.dart
@@ -22,7 +22,6 @@ class DBProvider {
 
   /// initialize the database
   Future initDB() async {
-    // Directory directory = await getApplicationDocumentsDirectory();
     final dbPath = await getDatabasesPath();
     String path = join(dbPath, 'hadith_db.db');
 
@@ -30,29 +29,32 @@ class DBProvider {
     bool exists = await databaseExists(path);
 
     // If the database doesn't exist, copy it from assets
-    if (exists) {
-      if (kDebugMode) {
-        print('DB already exists');
-      }
-      await openDatabase(path);
-    } else {
+    if (!exists) {
       if (kDebugMode) {
         print('Creating a copy from assets');
       }
-    }
+      try {
+        // Create the directory if it doesn't exist
+        await Directory(dirname(path)).create(recursive: true);
 
-    try {
-      await Directory(dirname(path)).create(recursive: true);
-    } catch (_) {
-      ByteData data = await rootBundle.load(AssetsPath.database);
-      List<int> bytes =
-          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-      await File(path).writeAsBytes(bytes, flush: true);
-      if (kDebugMode) {
-        print('DB copied');
+        // Copy the database from assets
+        ByteData data = await rootBundle.load(AssetsPath.database);
+        List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+        await File(path).writeAsBytes(bytes, flush: true);
+
+        if (kDebugMode) {
+          print('DB copied');
+        }
+      } catch (e) {
+        if (kDebugMode) {
+          print('Error copying database: $e');
+        }
       }
     }
 
-    await openDatabase(path);
+    // Open the database
+    _database = await openDatabase(path);
   }
+
 }
